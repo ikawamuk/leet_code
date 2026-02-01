057a6f362115dc960b3eefac688dec67fd4d7270
ikawamuk <ikawamuk@student.42.fr>
2026-02-01 20:37:23 +0900

    update:

---
+++ MaximumDepthofBinaryTree/step1.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+
+
+int maxDepth(struct TreeNode* root)
+{
+	int	depth = 0;
+
+	return (get_depth(root, depth));
+}
+
+int	get_depth(struct TreeNode* node, int depth)
+{
+	if (!node)
+		return (depth);
+	int	left_depth = get_depth(node->left, depth + 1);
+	int	right_depth = get_depth(node->right, depth + 1);
+	return (left_depth < right_depth ? right_depth : left_depth);
+}
